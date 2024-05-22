da48b9bfde6c3b9adf90cd9d19c36e9fba287c56
rakeshdevops92 <51206514+rakeshdevops92@users.noreply.github.com>
2024-05-22 19:43:39 +0530

    Update json-to-txt.ps1

--- json-to-txt.ps1
+++ json-to-txt.ps1
@@ -2,9 +2,6 @@
 $jsonFilePath = "path\to\your\jsonfile.json"
 $jsonContent = Get-Content -Raw -Path $jsonFilePath | ConvertFrom-Json
 
-# Output file path
-$outputFilePath = "path\to\your\outputfile.txt"
-
 # Initialize variables for storing max lengths
 $maxNameLength = 0
 $maxVersionLength = 0
@@ -23,6 +20,10 @@ foreach ($key in $jsonContent.imports.Keys) {
     }
 }
 
+# Ensure that the padding values are not negative
+$namePadding = [math]::Max($maxNameLength - 17, 0)
+$versionPadding = [math]::Max($maxVersionLength - 7, 0)
+
 # Create a formatted date string
 $dateTime = Get-Date -Format "MM/dd/yyyy HH:mm:ss"
 $buildVersion = "34.12421.1475.0"  # Replace with actual build version if needed
@@ -30,17 +31,18 @@ $buildVersion = "34.12421.1475.0"  # Replace with actual build version if needed
 # Initialize output string
 $output = "HPX Build Date: $dateTime`n"
 $output += "HPX Build Version: $buildVersion`n`n"
-$output += "| PACKAGE NAME (features) " + " " * ($maxNameLength - 17) + "| VERSION " + " " * ($maxVersionLength - 7) + "|`n"
+$output += "| PACKAGE NAME (features) " + " " * $namePadding + "| VERSION " + " " * $versionPadding + "|`n"
 $output += "|" + "-" * ($maxNameLength + 1) + "|" + "-" * ($maxVersionLength + 1) + "|`n"
 
 # Append features to output string
 foreach ($key in $jsonContent.imports.Keys) {
     $name = $key
     $version = $jsonContent.imports.$key
-    $output += "| $name" + " " * ($maxNameLength - $name.Length + 1) + "| $version" + " " * ($maxVersionLength - $version.Length + 1) + "|`n"
+    $output += "| $name" + " " * ([math]::Max($maxNameLength - $name.Length + 1, 0)) + "| $version" + " " * ([math]::Max($maxVersionLength - $version.Length + 1, 0)) + "|`n"
 }
 
-
+# Output file path
+$outputFilePath = "path\to\your\outputfile.txt"
 Set-Content -Path $outputFilePath -Value $output
 
 Write-Host "Human-readable text file has been created at $outputFilePath"
