5d1fac6fb8158eb045caa4cea216279aef00b06d
rakeshdevops92 <51206514+rakeshdevops92@users.noreply.github.com>
2024-05-22 20:18:29 +0530

    Update json-to-txt.ps1

--- json-to-txt.ps1
+++ json-to-txt.ps1
@@ -1,18 +1,20 @@
-# Read JSON file and parse it
-$jsonFilePath = ".\importmap.json"
+param (
+    [string]$jsonFilePath,
+    [string]$outputFilePath,
+    [string]$buildVersion
+)
+
 $jsonContent = Get-Content -Raw -Path $jsonFilePath | ConvertFrom-Json
 
-# Verify the structure of the JSON
+# Verifying the structure of the JSON
 if (-not $jsonContent.imports) {
     Write-Host "The 'imports' property is not found in the JSON content."
     exit 1
 }
 
-# Initialize variables for storing max lengths
 $maxNameLength = 0
 $maxVersionLength = 0
 
-# Determine max lengths for formatting
 foreach ($key in $jsonContent.imports.PSObject.Properties.Name) {
     $nameLength = $key.Length
     $versionLength = $jsonContent.imports.$key.Length
@@ -26,29 +28,21 @@ foreach ($key in $jsonContent.imports.PSObject.Properties.Name) {
     }
 }
 
-# Ensure the lengths are at least as long as the header titles
 $maxNameLength = [math]::Max($maxNameLength, 20)
 $maxVersionLength = [math]::Max($maxVersionLength, 7)
 
-# Create a formatted date string
 $dateTime = Get-Date -Format "MM/dd/yyyy HH:mm:ss"
-$buildVersion = "34.12421.1475.0"  # Replace with actual build version if needed
 
-# Initialize output string
 $output = "HPX Build Date: $dateTime`n"
 $output += "HPX Build Version: $buildVersion`n`n"
 $output += "| PACKAGE NAME (features) " + " " * ($maxNameLength - 20) + "| VERSION " + " " * ($maxVersionLength - 7) + "|`n"
 $output += "|" + "-" * ($maxNameLength + 2) + "|" + "-" * ($maxVersionLength + 8) + "|`n"
 
-# Append features to output string
 foreach ($key in $jsonContent.imports.PSObject.Properties.Name) {
     $name = $key
     $version = $jsonContent.imports.$key
     $output += "| " + $name + " " * ($maxNameLength - $name.Length + 1) + "| " + $version + " " * ($maxVersionLength - $version.Length + 1) + "|`n"
 }
 
-# Output file path
-$outputFilePath = ".\outputfile.txt"
 Set-Content -Path $outputFilePath -Value $output -Force
-
 Write-Host "Human-readable text file has been created at $outputFilePath"
