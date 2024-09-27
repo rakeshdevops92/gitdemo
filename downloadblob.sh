a083934e231a58abfe51dccf504542589262a093
rakeshdevops92 <51206514+rakeshdevops92@users.noreply.github.com>
2024-09-27 20:31:02 +0530

    Create downloadblob.sh

---
+++ downloadblob.sh
@@ -0,0 +1,20 @@
+STORAGE_ACCOUNT_NAME="${storageAccountName}"
+CONTAINER_NAME="test"
+STORAGE_ACCOUNT_KEY="${storageAccountKey}"
+BLOB_NAME="config.xml"                 # Name of the file in the storage container
+LOCAL_PATH="/path/to/config.xml"        # Path to place the file on the VM
+
+# Download config.xml from the storage account to the VM
+echo "Downloading config.xml from the storage account..."
+az vm run-command invoke \
+  --resource-group ${midrgname} \
+  --name ${midvmname} \
+  --command-id RunShellScript \
+  --scripts "sudo su && az storage blob download --account-name ${STORAGE_ACCOUNT_NAME} --account-key '${STORAGE_ACCOUNT_KEY}' --container-name ${CONTAINER_NAME} --name ${BLOB_NAME} --file ${LOCAL_PATH}"
+
+if [ $? -eq 0 ]; then
+  echo "File downloaded successfully to ${LOCAL_PATH}!"
+else
+  echo "File download failed."
+  exit 1
+fi
