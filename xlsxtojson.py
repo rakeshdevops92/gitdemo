6de97dabbd93cdd55f12397548179c2c34200c81
rakeshdevops92 <51206514+rakeshdevops92@users.noreply.github.com>
2024-10-13 09:58:36 +0530

    Update xlsxtojson.py

--- xlsxtojson.py
+++ xlsxtojson.py
@@ -7,16 +7,24 @@ src_file = "/home/pj72963/metadata_excel/Content_Metadata_by_Cost_Center.xlsx"
 dest_file = "/home/pj72963/metadata_json/output.json"
 
 def excel_to_json():
-    df = pd.read_excel(src_file)
+    df = pd.read_excel(src_file, header=None)
     
-    metadata_keys = df.iloc[0:7, 0].values
-    metadata_values = df.iloc[0:7, 1].values
+    metadata = {}
+    for i in range(df.shape[0]):
+        if pd.isna(df.iloc[i, 0]) and pd.isna(df.iloc[i, 2]):
+            break
+        if pd.notna(df.iloc[i, 0]) and pd.notna(df.iloc[i, 1]):
+            metadata[df.iloc[i, 0]] = df.iloc[i, 1]
+        if pd.notna(df.iloc[i, 2]) and pd.notna(df.iloc[i, 3]):
+            metadata[df.iloc[i, 2]] = df.iloc[i, 3]
 
-    metadata = {metadata_keys[i]: metadata_values[i] for i in range(len(metadata_keys))}
+    metadata = {k: v for k, v in metadata.items() if pd.notna(v)}
 
     relation_id = hashlib.md5(str(metadata).encode()).hexdigest()
 
-    df_files = pd.read_excel(src_file, skiprows=7)
+    file_data_start_row = i + 2
+
+    df_files = pd.read_excel(src_file, skiprows=file_data_start_row)
     df_files.fillna("", inplace=True)
 
     json_output = []
@@ -28,13 +36,14 @@ def excel_to_json():
     })
 
     for index, row in df_files.iterrows():
-        file_metadata = {col_name: row[col_name] for col_name in df_files.columns}
-
-        json_output.append({
-            "operation": "upload_new_file",
-            "relation_id": relation_id,
-            "file_metadata": file_metadata
-        })
+        file_metadata = {col_name: row[col_name] for col_name in df_files.columns if pd.notna(row[col_name]) and row[col_name] != ""}
+        
+        if file_metadata:
+            json_output.append({
+                "operation": "upload_new_file",
+                "relation_id": relation_id,
+                "file_metadata": file_metadata
+            })
 
     with open(dest_file, 'w') as json_file:
         json.dump(json_output, json_file, indent=4)
