d9aa53791b829e6d11a6a1ab4b00951cbedb90d7
rakeshdevops92 <51206514+rakeshdevops92@users.noreply.github.com>
2024-12-21 00:13:25 +0530

    Update updated.ps1

--- updated.ps1
+++ updated.ps1
@@ -1,7 +1,7 @@
 # Update DesktopExtension.csproj References
-(Get-Content $DesktopCsproj) -replace "($NugetName.*?, Version=)[0-9\.]+", "$1$NewVersion" -replace "(HintPath.*?$NugetName\\.*?\\)(.*?)(\")", "$1$NewVersion$3" | Set-Content $DesktopCsproj
+(Get-Content $DesktopCsproj) -replace "($NugetName.*?, Version=)[0-9\.]+", "`$1$NewVersion" -replace "(HintPath.*?$NugetName\\.*?\\).*?(\")", "`$1$NewVersion`$2" | Set-Content $DesktopCsproj
 Write-Host "Successfully updated $NugetName references in $DesktopCsproj"
 
 # Update HP.HPX.csproj Content References
-(Get-Content $HpxCsproj) -replace "(<Content Include=\".*?$NugetName\\.*?\\)(.*?)(\")", "$1$NewVersion$3" | Set-Content $HpxCsproj
+(Get-Content $HpxCsproj) -replace "(<Content Include=\".*?$NugetName\\.*?\\).*?(\")", "`$1$NewVersion`$2" | Set-Content $HpxCsproj
 Write-Host "Successfully updated $NugetName content references in $HpxCsproj"
